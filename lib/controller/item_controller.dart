3955100ebf427bf58bf0adb598cb554c4d10d279
DhanashreeDD <dhanashreeflutter@gmail.com>
2024-09-06 17:24:49 +0530

    model class completed

--- lib/controller/item_controller.dart
+++ lib/controller/item_controller.dart
@@ -1,6 +1,7 @@
 import '../model/items_model.dart';
 
-List<ItemsModel> itemList = [
+class ItemsData{
+  List<ItemsModel> itemList = [
   ItemsModel(
     camImg: "assets/images/img1.png",
     camName: "Canon EOS 30D",
@@ -40,3 +41,5 @@ List<ItemsModel> itemList = [
    
   
 ];
+
+}
